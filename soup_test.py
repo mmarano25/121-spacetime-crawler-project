4081fc4fc9e805c2cb8e4c9c2421504783ce117b
mmarano25 <mmarano@uci.edu>
2018-04-30 20:36:14 -0700

    Modified soup_test to work on lxml

--- soup_test.py
+++ soup_test.py
@@ -1,9 +1,14 @@
 from bs4 import BeautifulSoup
+from lxml.html import parse, make_links_absolute, tostring
 
 url = "/home/matt/Desktop/documentation/python3.6/python-3.6.3-docs-html/installing/index.html"
 with open(url, "r") as html:
-    soup = BeautifulSoup(html, "lxml")
-    # print soup.prettify()
+    html2 = parse(html)
+    html2 = make_links_absolute(html2.getroot(), url)
+    html2 = tostring(html2)
+    
+    soup = BeautifulSoup(html2, "lxml")
+    print soup.prettify()
     links = soup.find_all("a")
     for l in links:
         print l['href']
